978ee698accdb572f49b5b8b26347536491037cb
zawlinn-dev <zawlinn.designer@gmail.com>
2022-07-20 19:27:26 +0700

    JSON Server : Basic Setup

---
+++ data/product.js
@@ -0,0 +1,19 @@
+module.exports = () =>{
+
+    const data = {
+
+        products: [],
+
+    }
+    for(let i = 0; i < 100; i++){
+
+        data.products.push({
+
+            id: i,
+            title: `products${i}`
+        })
+    }
+
+    return data
+    
+}
